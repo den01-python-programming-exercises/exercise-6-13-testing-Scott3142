de96e13b5331f8659491a775c4f0b8bda2865a0d
Scott Morgan <smorgan@bridgend.ac.uk>
2021-01-26 08:39:58 +0000

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -3,4 +3,8 @@ import src.exercise
 
 def test_exercise():
     #implement tests here
-    
+    assert src.exercise.divide(3,4) == 0.75
+    assert src.exercise.summation(3,4) == 7
+
+    assert src.exercise.divide(3,-4) == -0.75
+    assert src.exercise.summation(-3,4) == 1
